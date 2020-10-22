9dfc96554f3a5051044e9900b840f533bac25a35
Mikail <mikail@poshmark.com>
2020-10-22 13:18:51 -0700

    monthly metrics added

---
+++ models/monthly_metrics/monthly_growth_metrics_us.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+SELECT
+    TO_CHAR(date_trunc('month', daily_growth_metrics.date), 'YYYY-MM') as month,
+    COALESCE(SUM(daily_growth_metrics.sales ), 0) AS "sales",
+	COALESCE(SUM(daily_growth_metrics.sold_items ), 0) AS "sold_items",
+    COALESCE(SUM(daily_growth_metrics.facebook_spend ), 0) AS "facebook_spend",
+    COALESCE(SUM(daily_growth_metrics.google_spend ), 0) AS "google_spend"
+FROM {{ref('daily_growth_metrics_us')}}  AS daily_growth_metrics
+group by 1
