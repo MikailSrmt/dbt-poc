0f039cae608007af26207212e22360fb5da1cd27
Mikail <mikail@poshmark.com>
2020-10-22 19:41:31 -0700

    More housekeeping

--- models/views/stg_sales_with_sold_items_us.sql
+++ models/views/stg_sales_with_sold_items_us.sql
@@ -1,5 +1,4 @@
 {{ config(materialized='view') }}
-{{ config(materialized='table') }}
 select sales.date, sales.sales, sold_items.sold_items
 from {{ref('daily_sales_us')}} as sales left outer join {{ref('daily_sold_items_us')}} as sold_items on sales.date=sold_items.date
 
